d676a424bb052064c0e6b51eddf2dd7139757f03
Evgeniy <fisher.87@bk.ru>
2023-01-31 23:47:21 +0300

    created_README

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array1 = new string[] { "Mazda", "Kia", "BMW", "Lada", "47", ":-)", "ОГО" };
+
+string[] CreateSecondArray(string[] array1)         // метод заполнения второго массива из содержимого первого 
+{                                                   // массива согласно условиям задачи
+    string[] array2 = new string[array1.Length];
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return array2;
+}
+void ShowArray(string[] array)      // метод вывода полученного массива
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+string[] myArray = CreateSecondArray(array1);
+ShowArray(myArray);
